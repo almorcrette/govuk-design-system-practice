7ef86670ba14765fa5982cd2351f8a5a1cf547e8
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-09-01 14:24:43 +0100

    implement: add fetch boards from api in where-you-live route

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+var request = require('then-request');
+
 // Add your routes here - above the module.exports line
+router.get('/where-you-live', function(req, res) {
+    request('GET', 'http://localhost:9000/boards').done( boardsRes => {
+        res.render('where-you-live.html', {
+            boards: boardsRes.getBody(),
+            testData: "Some test data",
+            testArray: [
+                "firstElement",
+                "secondElement",
+                "thirdElement"
+            ]
+        })
+    })
+})
 
 module.exports = router
